2af6baa6a21db5470d6d00d47665612c5b27171d
강동윤 <kdy1@outlook.kr>
2018-10-13 10:54:43 +0900

    fixup! Fixes #24

--- ios/Classes/MapViewController.h
+++ ios/Classes/MapViewController.h
@@ -8,7 +8,6 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 #import <GoogleMaps/GoogleMaps.h>
-#import <GoogleMaps/GoogleMaps/GMSIndoorDisplay.h>
 
 @class MapViewPlugin;
 @class MapAnnotation;
